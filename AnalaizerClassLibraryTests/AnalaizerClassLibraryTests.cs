829608fe70d72cce5f57b007aa1a5cd35ebaa341
Sveta <Sveta@PC-LITE>
2022-11-30 13:36:56 +0200

    Add project files.

---
+++ AnalaizerClassLibraryTests/AnalaizerClassLibraryTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnalaizerClassLibrary;
+using System;
+
+namespace AnalaizerClassTests
+{
+    [TestClass]
+    public class ReplaceUnaryPlusMinusTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", @"C:\Users\User\Documents\Calculator\AnalaizerClassLibraryTests\TestData.xml", "Test", DataAccessMethod.Sequential)]
+
+        [TestMethod]
+
+        public void InputString_ReplaceUnaryPlusMinusTest_ChangedString()
+        {
+
+            // arrange
+            string input = (string)TestContext.DataRow["input"];
+            string expected = (string)(TestContext.DataRow["expected"]);
+
+            // act
+            string result;
+
+            // assert
+            try
+            {
+                result = AnalaizerClass.ReplaceUnaryPlusMinus(input);
+                Assert.AreEqual(expected, result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ AnalaizerClassLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalaizerClassLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnalaizerClassLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14ef6aad-0ca1-41a2-908d-1e25c869f463")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
